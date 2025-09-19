eb060b50218a2e634415860deb5f43418d327080
IllyaShramko <123illya123123r@gmail.com>
2025-09-19 22:54:05 +0300

    Added express framework for start up localhost

--- js1.js
+++ js1.js
@@ -1,6 +1,24 @@
-const moment = require("moment")
+const express = require("express");
+const moment = require("moment");
+
+const app = express();
+
+const PORT = 8000;
+const HOST = "localhost";
 
 function getDate() {
-    console.log(moment().format("YYYY/MM/DD HH:mm:ss"))
+    return moment().format("YYYY/MM/DD HH:mm:ss");
 }
-getDate()
+
+app.get("/", (req, res) => {
+    res.json("hello");
+});
+
+app.get("/timestamp", (req, res) => {
+    const currentTime = getDate();
+    res.json({ timestamp: currentTime });
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Server started on http://${HOST}:${PORT}`);
+});
