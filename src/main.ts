21d3bb6254d8522c77cbf5fdfcf0b102ae8512c7
IllyaShramko <123illya123123r@gmail.com>
2025-12-19 21:06:02 +0200

    Added allow localhost:3000

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import express from 'express'
 import { PostRouter } from './Post/post.routes'
 import { TagsRouter } from './Tag/tag.routes'
 import { UserRouter } from './User/user.routes'
+import cors from 'cors'
 const app: express.Express = express()
 
 app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}))
 app.use(PostRouter)
 app.use(TagsRouter)
 app.use(UserRouter)
